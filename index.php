8148e47638910c046336785e7b199c1ee877efaa
Dremof <oblizin.vip@yandex.ru>
2021-08-25 15:23:03 +0300

    add configBD and more fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   </head>
   <body>
-    <table class="table table-bordered">
+    <table class="table table-bordered" >
   <thead>
     <tr>
       <th scope="col">#</th>
